c81fa2b0d95d37e236c6f68cadc59f1be852cdc8
Libin <libinthankayathil@gmail.com>
2025-06-15 00:32:14 +0530

    final commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Icon from '../assets/search.svg'
+
+const Search = ({ searchTerm, setSearchTerm}) => {
+  return (
+    <div className='search'>
+        <div>
+            <img src={Icon} alt="" />
+            <input 
+                type="text"
+                placeholder='Search through thousands of anime'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+             />
+        </div>
+    </div>
+  )
+}
+
+export default Search
